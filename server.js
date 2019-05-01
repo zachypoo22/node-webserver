49edad0254f450012e0798d8d5350d3ae84f7823
zachm93@live.com <zachm93@live.com>
2019-05-01 15:58:26 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server log', log + ' \n', (err) => {
+        if (err) {
+            console.log('unable to append to server.log');
+        }
+    });
+
+    next();
+});
+
+// app.use((req, res, next) => {
+//   res.render(__dirname + '/views/maint.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render(__dirname + '/views/home.hbs', {
+        pageTitle: 'Home',
+        welcomeMessage: 'Hi there!',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render(__dirname + '/views/about.hbs', {
+        pageTitle: 'About page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'OH NO THIS BAD!'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server up.');
+});
